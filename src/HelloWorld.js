3a8f37865c8a5ca9c8d9477ad4b8b0109ae0f4b2
jihaneh <jijih@hotmail.fr>
2022-05-31 06:46:43 +0200

    Add homepage + reducer

---
+++ src/HelloWorld.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getHelloDispatcher } from './helloworld/hello';
+
+export default function HelloWorld() {
+  const dispatch = useDispatch();
+
+  const hello = useSelector((state) => state.helloReducer.greeting || '');
+
+  const handleClick = () => {
+    dispatch(getHelloDispatcher());
+  };
+
+  return (
+    <>
+      <h1>
+        {' '}
+        Greeting:
+        {hello}
+      </h1>
+      <button className="helloBtn" onClick={handleClick}>
+        Get Random
+      </button>
+    </>
+  );
+}
